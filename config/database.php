1ae6b25ae064f5a73d960a25943a09e51c48afce
amineDev <maaouidane@anypli.com>
2016-03-31 11:18:32 +0100

    add vendor

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
       'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'chat_server'),
+            'database'  => env('DB_DATABASE', 'elearning'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
